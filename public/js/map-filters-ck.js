0011783ed87ecbcd322fcd72693ffb2d6de56395
Cory Gwin <gwincr11@yahoo.com>
2013-03-08 14:12:22 -0600

    map filters

---
+++ public/js/map-filters-ck.js
@@ -0,0 +1,1 @@
+var mapFilters={form:$("#filters form"),init:function(){$("#filternav a").click(function(e){e.preventDefault();mapFilters.show();$(this).tab("show");$(".close").click(function(){mapFilters.hide()})})},hide:function(){$("#filters form").fadeOut("fast")},show:function(){this.form.removeClass("hide");this.form.fadeIn("fast")}};mapFilters.init();
