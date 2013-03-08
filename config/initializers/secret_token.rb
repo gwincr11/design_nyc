7db7f6ad0bcbe94dcf2d318b778cac5bf70c577e
Cory Gwin <gwincr11@yahoo.com>
2013-03-08 10:12:32 -0600

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DesignNyc::Application.config.secret_token = '00afd7eac32c37fb0c4e132fc3450a64e571d99e23a9194b5d0141e4d5c59f9a474229e2a94c5bf0f0d6ce048e20ad7e883d11d85dc26baeb1d838133e810f70'
