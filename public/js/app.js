42988b9154750a3bcaf3be4d7cf7e4fb5b578dea
Tom Zellman <tzellman@gmail.com>
2013-03-08 13:04:45 -0500

    removed initialize

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,46 @@
-var mapOptions = {
-    center: new google.maps.LatLng(40.7142, -74.0064),
-    zoom: 16,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-var map = new google.maps.Map(document.getElementById("map_canvas"),
-    mapOptions);
+(function(){
+
+
+    var markers = {}
+    var infowindow = new google.maps.InfoWindow();
+
+    function drawMap(lat,long){
+        var mapOptions = {
+            zoom: 16,
+            center: new google.maps.LatLng(lat, long),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    }
+
+
+    function addMarker(latLong, name, title, letter){
+        markers[name] = new google.maps.Marker({
+            position: latLong,
+            map: map,
+            title: title,
+            id:name,
+            icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld='+letter+'|1DAFEC|ffffff'
+        });
+        google.maps.event.addListener(markers[name], 'click', function() {
+            map.setCenter(markers[name].getPosition());
+            map.setZoom(13);
+            fadeInStore(markers[name]["id"]);
+            var el = $("#"+ name);
+            var address = el.find("address").html();
+            infowindow.setContent('<h2>' + title + '</h2><br />' + address);
+            infowindow.open(map, markers[name]);
+        });
+    }
+
+
+    $("document").ready(function(){
+        drawMap("40.7142", "-74.0064");
+
+        // TODO add markers to map
+
+
+    });
+
+})();
+
